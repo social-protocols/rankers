c77c3198a6d705df049d4343d69e481c0f8055a3
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-12-03 14:43:33 +0100

    feat: setup simulated post and vote stream

--- simulation/simulation.jl
+++ simulation/simulation.jl
@@ -14,12 +14,13 @@ endpoints = Dict(
 )
 
 comment_probability = 0.8
+post_to_vote_ratio = 0.05
 
-post_id_buffer = Int[]
+post_buffer = Int[]
+vote_event_buffer = Int[]
 
-function create_post!(buf::Array{Int}, host::String, endpoint::String, comment_probability::Float64)
-    post_id = isempty(buf) ? 1 : maximum(buf) + 1
-    parent_id = rand() < comment_probability && isempty(buf) ? nothing : rand(buf)
+function create_post!(post_buf::Array{Int}, host::String, endpoint::String, comment_probability::Float64)
+    (post_id, parent_id) = isempty(post_buf) ? (1, nothing) : (maximum(post_buf) + 1, rand(post_buf))
 
     post = Dict(
         "post_id" => post_id,
@@ -29,7 +30,7 @@ function create_post!(buf::Array{Int}, host::String, endpoint::String, comment_p
     )
     headers = Dict("Content-Type" => "application/json")
 
-    println("Attempting to create post", post)
+    println("Creating post: ", post_id, "...")
 
     HTTP.post(
         host_url * endpoint,
@@ -37,15 +38,47 @@ function create_post!(buf::Array{Int}, host::String, endpoint::String, comment_p
         body=JSON.json(post)
     )
 
-    push!(buf, post_id)
+    push!(post_buf, post_id)
 end
 
-for i in 1:100
-    create_post!(
-        post_id_buffer,
-        host_url,
-        endpoints[:create_post],
-        comment_probability,
+function send_vote_event!(vote_event_buf::Array{Int}, post_buf::Array{Int}, host::String, endpoint::String)
+    vote_event_id = isempty(vote_event_buf) ? 1 : maximum(vote_event_buf) + 1
+    post_id = rand(post_buf)
+
+    vote_event = Dict(
+        "vote_event_id" => vote_event_id,
+        "post_id" => post_id,
+        "vote" => 1,
+        "vote_event_time" => Int(floor(Dates.time())),
     )
+    headers = Dict("Content-Type" => "application/json")
+
+    println("Creating vote event: ", vote_event_id, "...")
+
+    HTTP.post(
+        host_url * endpoint,
+        headers,
+        body=JSON.json(vote_event)
+    )
+
+    push!(vote_event_buf, vote_event_id)
+end
+
+for i in 1:1000
+    if (rand() > post_to_vote_ratio) && !isempty(post_buffer)
+        send_vote_event!(
+            vote_event_buffer,
+            post_buffer,
+            host_url,
+            endpoints[:send_vote_event],
+        )
+    else
+        create_post!(
+            post_buffer,
+            host_url,
+            endpoints[:create_post],
+            comment_probability,
+        )
+    end
     sleep(1)
 end
