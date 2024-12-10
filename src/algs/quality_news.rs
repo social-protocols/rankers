feb71e2e00d70d5cc302432defa2d2c7b8f89921
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-12-10 20:02:18 +0100

    refactor: sampling stats logic
    - small fix: estimation of upvote share (default model)

--- src/algs/quality_news.rs
+++ src/algs/quality_news.rs
@@ -45,9 +45,6 @@ pub async fn get_ranking(tx: &mut Transaction<'_, Sqlite>) -> Result<Vec<ScoredI
 pub async fn sample_stats(
     tx: &mut Transaction<'_, Sqlite>,
 ) -> Result<axum::http::StatusCode, AppError> {
-    let sample_time = now_utc_millis();
-    info!("Sampling stats at: {:?}", sample_time);
-
     if let Err(_) = query_scalar::<_, i32>("select 1 from item limit 1")
         .fetch_one(&mut **tx)
         .await
@@ -56,34 +53,59 @@ pub async fn sample_stats(
         return Ok(axum::http::StatusCode::OK);
     }
 
-    if let Err(_) = query_scalar::<_, i32>("select 1 from rank_history limit 1")
-        .fetch_one(&mut **tx)
-        .await
+    let sample_time = now_utc_millis();
+    info!("Sampling stats at: {:?}", sample_time);
+
+    match query_scalar("select max(sample_time) from stats_history")
+        .fetch_optional(&mut **tx)
+        .await?
     {
-        info!("No ranks recorded yet - Initializing...");
+        // initialize stats and ranks histories
+        None => {
+            let items = repository::get_items_in_pool(tx).await?;
 
-        // TODO: come up with a better initialization logic
-        repository::initialize_rank_history(tx, sample_time).await?;
-        return Ok(axum::http::StatusCode::OK);
+            let mut initial_stats = Vec::<Observation<QnStats>>::new();
+            for item in &items {
+                let init_observation = Observation {
+                    sample_time,
+                    data: QnStats {
+                        item_id: item.item_id,
+                        submission_time: item.created_at,
+                        upvotes: repository::get_current_upvote_count(tx, item.item_id).await?,
+                        upvote_share: 0.0,
+                        expected_upvotes: 0.0,
+                    },
+                };
+                initial_stats.push(init_observation);
+            }
+            repository::insert_stats_observations(tx, &initial_stats).await?;
+
+            let initial_ranks = calc_updated_ranks(&initial_stats);
+            repository::insert_rank_observations(tx, &initial_ranks).await?;
+        }
+        // update stats and ranks histories
+        Some(previous_sample_time) => {
+            let previous_stats: Vec<Observation<QnStats>> =
+                repository::get_stats(&mut *tx, previous_sample_time).await?;
+
+            let n_items = previous_stats.len() as i32;
+
+            let sitewide_upvotes =
+                repository::get_sitewide_new_upvotes(&mut *tx, sample_time, previous_sample_time)
+                    .await?;
+
+            let mut updated_stats = Vec::<Observation<QnStats>>::new();
+            for stat in previous_stats {
+                let updated_stat: Observation<QnStats> =
+                    calc_updated_stats(tx, sample_time, stat, sitewide_upvotes, n_items).await?;
+                updated_stats.push(updated_stat);
+            }
+            repository::insert_stats_observations(tx, &updated_stats).await?;
+
+            let new_ranks = calc_updated_ranks(&updated_stats);
+            repository::insert_rank_observations(tx, &new_ranks).await?;
+        }
     }
-
-    let previous_sample_time: i64 =
-        sqlx::query_scalar("select max(sample_time) from stats_history")
-            .fetch_one(&mut **tx)
-            .await?;
-
-    let previous_stats: Vec<Observation<QnStats>> =
-        repository::get_stats(&mut *tx, previous_sample_time).await?;
-
-    let sitewide_upvotes =
-        repository::get_sitewide_new_upvotes(&mut *tx, previous_sample_time).await?;
-
-    let updated_stats: Vec<Observation<QnStats>> =
-        calc_updated_stats(tx, sample_time, previous_stats, sitewide_upvotes).await?;
-    repository::insert_stats_observations(tx, &updated_stats).await?;
-
-    let new_ranks = calc_updated_ranks(&updated_stats);
-    repository::insert_rank_observations(tx, &new_ranks).await?;
 
     Ok(axum::http::StatusCode::OK)
 }
@@ -91,42 +113,40 @@ pub async fn sample_stats(
 async fn calc_updated_stats(
     tx: &mut Transaction<'_, Sqlite>,
     sample_time: i64,
-    previous_stats: Vec<Observation<QnStats>>,
+    previous_stat: Observation<QnStats>,
     sitewide_upvotes: i32,
-) -> Result<Vec<Observation<QnStats>>, AppError> {
-    let items = repository::get_items_in_pool(tx).await?;
-    let expected_upvote_share_by_item = repository::get_expected_upvote_share_by_item(&items);
-    let upvote_count_by_item = repository::get_current_upvote_count_by_item(&mut *tx).await?;
+    n_items: i32,
+) -> Result<Observation<QnStats>, AppError> {
+    if sitewide_upvotes == 0 {
+        return Ok(Observation {
+            sample_time,
+            data: QnStats {
+                item_id: previous_stat.data.item_id,
+                submission_time: previous_stat.data.submission_time,
+                upvotes: previous_stat.data.upvotes,
+                upvote_share: 0.0,
+                expected_upvotes: 0.0,
+            },
+        });
+    }
 
-    let updated_stats: Vec<Observation<QnStats>> = previous_stats
-        .iter()
-        .map(|stat| {
-            let expected_upvote_share = expected_upvote_share_by_item
-                .get(&stat.data.item_id)
-                .unwrap(); // TODO
-            let new_upvotes = *upvote_count_by_item.get(&stat.data.item_id).unwrap(); // TODO
-            let new_expected_upvotes =
-                stat.data.expected_upvotes + (expected_upvote_share * (sitewide_upvotes as f32));
-            let actual_upvote_share = if sitewide_upvotes != 0 {
-                new_upvotes as f32 / sitewide_upvotes as f32
-            } else {
-                0.0
-            };
+    let expected_upvote_share = repository::get_expected_upvote_share(n_items);
+    let new_upvotes = repository::get_current_upvote_count(tx, previous_stat.data.item_id).await?;
+    let new_expected_upvotes =
+        previous_stat.data.expected_upvotes + (expected_upvote_share * (sitewide_upvotes as f32));
+    let actual_upvote_share =
+        (new_upvotes - previous_stat.data.upvotes) as f32 / sitewide_upvotes as f32;
 
-            Observation {
-                sample_time,
-                data: QnStats {
-                    item_id: stat.data.item_id,
-                    submission_time: stat.data.submission_time,
-                    upvotes: new_upvotes,
-                    upvote_share: actual_upvote_share,
-                    expected_upvotes: new_expected_upvotes,
-                },
-            }
-        })
-        .collect();
-
-    Ok(updated_stats)
+    Ok(Observation {
+        sample_time,
+        data: QnStats {
+            item_id: previous_stat.data.item_id,
+            submission_time: previous_stat.data.submission_time,
+            upvotes: new_upvotes,
+            upvote_share: actual_upvote_share,
+            expected_upvotes: new_expected_upvotes,
+        },
+    })
 }
 
 fn calc_updated_ranks(stats: &Vec<Observation<QnStats>>) -> Vec<Observation<ItemWithRanks>> {
--- src/algs/quality_news/repository.rs
+++ src/algs/quality_news/repository.rs
@@ -2,33 +2,31 @@ use crate::algs::quality_news::model::{ItemWithRanks, QnStats};
 use crate::common::error::AppError;
 use crate::common::model::{Item, Observation};
 use sqlx::{query, query_as, query_scalar, Sqlite, Transaction};
-use std::collections::HashMap;
 
-pub async fn get_current_upvote_count_by_item(
+pub async fn get_current_upvote_count(
     tx: &mut Transaction<'_, Sqlite>,
-) -> Result<HashMap<i32, i32>, AppError> {
-    let upvote_counts: Vec<(i32, i32)> = query_as(
+    item_id: i32,
+) -> Result<i32, AppError> {
+    let upvote_count: i32 = query_scalar(
         "
-        select ip.item_id, count(*)
+        select count(*)
         from item_pool ip
         join vote v
         on ip.item_id = v.item_id
         where vote = 1
+        and ip.item_id = ?
         ",
     )
-    .fetch_all(&mut **tx)
+    .bind(item_id)
+    .fetch_one(&mut **tx)
     .await?;
 
-    let mut upvote_count_by_item = HashMap::new();
-    for (item_id, upvote_count) in upvote_counts {
-        upvote_count_by_item.insert(item_id.clone(), upvote_count.clone());
-    }
-
-    Ok(upvote_count_by_item)
+    Ok(upvote_count)
 }
 
 pub async fn get_sitewide_new_upvotes(
     tx: &mut Transaction<'_, Sqlite>,
+    sample_time: i64,
     previous_sample_time: i64,
 ) -> Result<i32, AppError> {
     let sitewide_upvotes: i32 = query_scalar(
@@ -36,9 +34,11 @@ pub async fn get_sitewide_new_upvotes(
         select count(*)
         from vote
         where vote = 1
+        and created_at < ?
         and created_at > ?
         ",
     )
+    .bind(sample_time)
     .bind(previous_sample_time)
     .fetch_one(&mut **tx)
     .await?;
@@ -55,19 +55,8 @@ pub async fn get_items_in_pool(tx: &mut Transaction<'_, Sqlite>) -> Result<Vec<I
 }
 
 // TODO: create poisson regression model to estimate upvote share
-pub fn get_expected_upvote_share_by_item(items: &Vec<Item>) -> HashMap<i32, f32> {
-    if items.is_empty() {
-        return HashMap::new();
-    }
-
-    let default_upvote_share = 1.0 / items.len() as f32;
-
-    let mut upvote_share_by_item = HashMap::new();
-    for item in items.iter() {
-        upvote_share_by_item.insert(item.item_id, default_upvote_share);
-    }
-
-    upvote_share_by_item
+pub fn get_expected_upvote_share(n_items: i32) -> f32 {
+    1.0 / n_items as f32
 }
 
 pub async fn insert_stats_observations(
@@ -124,36 +113,11 @@ pub async fn insert_rank_observations(
     Ok(axum::http::StatusCode::OK)
 }
 
-pub async fn initialize_rank_history(
-    tx: &mut Transaction<'_, Sqlite>,
-    sample_time: i64,
-) -> Result<axum::http::StatusCode, AppError> {
-    query(
-        "
-        with ranked_item_pool as (
-            select
-                  item_id
-                , ? as sample_time
-                , row_number() over (order by created_at desc) as rank_top
-                , row_number() over (order by created_at desc) as rank_new
-            from item_pool
-        )
-        insert into rank_history
-        select * from ranked_item_pool
-        ",
-    )
-    .bind(sample_time)
-    .fetch_all(&mut **tx)
-    .await?;
-
-    return Ok(axum::http::StatusCode::OK);
-}
-
 pub async fn get_stats(
     tx: &mut Transaction<'_, Sqlite>,
     sample_time: i64,
 ) -> Result<Vec<Observation<QnStats>>, AppError> {
-    let stats_observations = sqlx::query_as::<_, QnStats>(
+    let stats_observations = query_as::<_, QnStats>(
         "
         with stats_at_sample_time as (
             select *
