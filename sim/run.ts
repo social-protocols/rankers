d1924d95112f4b5a29645614e6d865d433b085d7
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-12-13 13:15:55 +0100

    feat: finish basic simulation in typescript

--- sim/run.ts
+++ sim/run.ts
@@ -1,10 +1,18 @@
-import { objectToSnake } from 'ts-case-convert'
+import { objectToSnake, objectToCamel } from 'ts-case-convert'
+import * as PD from 'probability-distributions'
 
 type Item = {
   itemId: number
   parentId?: number
   authorId: string
   createdAt: number
+}
+
+type ScoredItem = {
+  itemId: number
+  rank: number
+  page: string
+  score: number
 }
 
 type VoteEvent = {
@@ -17,18 +25,35 @@ type VoteEvent = {
   createdAt: number
 }
 
-async function getRanking(): Promise<boolean> {
-  const url = 'http://localhost:3000/rankings/hn'
-  try {
-    const response = await fetch(url)
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`)
-    }
-  } catch (error) {
-    console.error('Fetching error:', error)
-    return false
+enum Page {
+  Newest = 'Newest',
+  HackerNews = 'HackerNews',
+  QualityNews = 'QualityNews',
+}
+
+enum Action {
+  PostItem,
+  PostVoteEvent,
+}
+
+async function getRanking(page: Page): Promise<Array<ScoredItem>> {
+  const endpoint = page === Page.Newest ?
+    '/rankings/newest' : (
+    page === Page.HackerNews ?
+      '/rankings/hn' :
+      '/rankings/qn'
+  )
+  const url = 'http://localhost:3000' + endpoint
+
+  const response = await fetch(url)
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`)
   }
-  return true
+
+  let ranking = objectToCamel(await response.json())
+
+  return ranking
 }
 
 async function postItem(item: Item): Promise<boolean> {
@@ -68,47 +93,57 @@ async function postVoteEvent(voteEvent: VoteEvent): Promise<boolean> {
   return true
 }
 
-enum Page {
-  Newest = 'Newest',
-  HackerNews = 'HackerNews',
-  QualityNews = 'QualityNews',
-}
-
-enum Action {
-  PostItem,
-  PostVoteEvent,
-  GetRanking,
-}
-
 function chooseAction() {
-  // TODO: randomize the choice (hard code probabilities)
-  return Action.PostItem
+  return PD.sample(
+    [Action.PostItem, Action.PostVoteEvent],
+    1,
+    true,
+    [0.05, 0.95]
+  )[0]
 }
 
-function chooseRank() {
-  // TODO: randomize the choice (Zipf's law)
-  return 1
+function chooseRank(nRanks: number): number {
+  const ranks = Array.from({ length: nRanks }, (_, index) => index + 1)
+
+  const probabilitiesUnnormalized = ranks.map(r => 1 / r)
+
+  const normalizationConstant =
+    probabilitiesUnnormalized.reduce((sum, current) => sum + current, 0)
+
+  const probabilities = probabilitiesUnnormalized.map((prob, i) => (1 / normalizationConstant) * (1 / (i + 1)))
+
+  return PD.sample(ranks, 1, true, probabilities)[0]
 }
 
-function chooseUser() {
-  // TODO: randomize the choice (uniform at random)
-  return 'user_1'
+function chooseUser(users: Array<number>): string {
+  const sample = users[Math.floor(Math.random() * users.length)]
+  return `user_${sample}`
 }
 
 function choosePage() {
-  // TODO: randomize the choice (hard code probabilities)
-  return Page.HackerNews
+  return PD.sample(
+    [Page.QualityNews, Page.HackerNews, Page.Newest],
+    1,
+    true,
+    [0.45, 0.45, 0.1]
+  )[0]
 }
 
-async function run() {
+const sleep = ms => new Promise(res => setTimeout(res, ms))
+
+async function main() {
   console.log('running simulation')
 
   let itemIdList: number[] = []
+  let voteEventIdList: number[] = []
+  let users = Array.from({ length: 100 }, (_, index) => index + 1)
+
+  // Seed posts
   for (let i = 0; i < 10; i++) {
     const itemId = itemIdList.length == 0 ? 1 : Math.max(...itemIdList) + 1
     const item = {
       itemId: itemId,
-      authorId: chooseUser(),
+      authorId: chooseUser(users),
       createdAt: Date.now(),
     }
     const success = await postItem(item)
@@ -119,7 +154,57 @@ async function run() {
     }
   }
 
+  // TODO: this is a bit hacky, there should be a better way to handle this
+  // Wait for quality news sampling to be initialized
+  await sleep(5000)
+
+  for (let i = 0; i < 100; i++) {
+    let action = chooseAction()
+    let user = chooseUser(users)
+    if (action == Action.PostItem) {
+      try {
+        const itemId = itemIdList.length == 0 ? 1 : Math.max(...itemIdList) + 1
+        const item = {
+          itemId: itemId,
+          authorId: user,
+          createdAt: Date.now(),
+        }
+        const success = await postItem(item)
+        if (success) {
+          itemIdList.push(itemId)
+          console.info('Submitted item:', itemId)
+        }
+      } catch {
+        console.error('Couldn\'t submit item')
+      }
+    } else {
+      try {
+        const voteEventId = voteEventIdList.length == 0 ? 1 : Math.max(...voteEventIdList) + 1
+        const page = choosePage()
+        const ranking = await getRanking(page)
+        const rank = chooseRank(ranking.length)
+        const voteEvent = {
+          voteEventId: voteEventId,
+          itemId: ranking[rank - 1].itemId,
+          userId: user,
+          vote: 1,
+          rank: rank,
+          page: page,
+          createdAt: Date.now(),
+        }
+        const success = await postVoteEvent(voteEvent)
+        if (success) {
+          voteEventIdList.push(voteEventId)
+          console.info('Submitted vote event:', voteEventId)
+        }
+      } catch {
+        console.error('Couldn\'t submit vote event')
+      }
+    }
+    await sleep(500)
+  }
+
   console.log('finishing simulation')
 }
 
-run()
+main()
